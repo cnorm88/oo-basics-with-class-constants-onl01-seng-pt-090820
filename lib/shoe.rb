cdfdb30d51ce2c355edaecb05c790df4e9cb4063
Crystal Norman <cnorm88@gmail.com>
2020-09-24 21:44:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(@brand) = true
+    if BRANDS.include?(@brand) != true
         BRANDS << @brand
     end
   end
@@ -17,7 +17,7 @@ class Shoe
   end
 
  def brands
-   if BRANDS.include?(@brand) = true
+   if BRANDS.include?(@brand) != true
    BRANDS << brands
    end
   end
